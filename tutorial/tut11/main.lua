8883d6b89beb196f6785d5378a0fb9e49a3263aa
Harsh Gaur <hgaur701@gmail.com>
2026-07-14 00:53:09 +0530

    learnt to create quad for spritesheet

--- tutorial/tut11/main.lua
+++ tutorial/tut11/main.lua
@@ -1,5 +1,29 @@
 function love.load()
-   image = love.graphics.newImage("jump.png")
+    frames = {}
+    local maxFrames = 5
+    local frame_width = 117
+    local frame_height = 233
+    image = love.graphics.newImage("images/jump_3.png")
+
+    for j = 0, 1, 1 do
+        for i = 0, 2, 1 do
+            table.insert(frames,
+                love.graphics.newQuad(
+                    1 + i * (frame_width + 2),   -- Starting x of quad
+                    1 + j * (frame_height + 2),  -- Starting y of quad
+                    frame_width,       -- width of quad
+                    frame_height,      -- height of quad
+                    image:getWidth(),  -- image width
+                    image:getHeight()) -- image height
+            )
+
+            if #frames == maxFrames then
+                break
+            end
+        end
+    end
+
+    currentFrame = 1
 end
 
 function love.update(dt)
@@ -11,5 +35,5 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.draw(frames[math.floor(currentFrame)])
+    love.graphics.draw(image, frames[math.floor(currentFrame)], 100, 100)
 end
