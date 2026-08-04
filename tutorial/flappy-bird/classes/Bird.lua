23c32ffd3e25fcebe08b7e6ceb4fb16a2c9dcfc4
Harsh Gaur <hgaur701@gmail.com>
2026-08-05 02:43:46 +0530

    Added gravity for the bird

--- tutorial/flappy-bird/classes/Bird.lua
+++ tutorial/flappy-bird/classes/Bird.lua
@@ -1,11 +1,24 @@
 Bird = Object:extend()
 
+local GRAVITY = 980
+
 function Bird:new(x, y)
     self.x = x or BASE_WIDTH / 2
     self.y = y or BASE_HEIGHT / 2
+    self.dy = 0
     self.image = love.graphics.newImage('images/bird.png')
     self.width = self.image:getWidth()
     self.height = self.image:getHeight()
+end
+
+function Bird:update(dt)
+    self.dy = self.dy + GRAVITY * dt
+
+    if love.keyboard.wasPressed('space') then
+        self.dy = -300
+    end
+
+    self.y = self.y + self.dy * dt
 end
 
 function Bird:draw()
