0017397d625a9c4701b8cab1e03dec5eab140e16
Harsh Gaur <hgaur701@gmail.com>
2026-09-05 19:33:24 +0530

    displaying empty heart and filled heart

--- games/breakout/classes/healthBar.lua
+++ games/breakout/classes/healthBar.lua
@@ -10,6 +10,8 @@ function HealthBar:new()
     self.remainingHearts = 3
     self.heartQuad = GenerateQuadsHearts(GTextures['main'], 128, 48)
     self.emptyHeartQuad = GenerateQuadsHearts(GTextures['main'], 138, 48)
+    local _, _, heartWidth = self.heartQuad:getViewport()
+    self.heartWidth = heartWidth
 end
 
 function HealthBar:render()
@@ -18,5 +20,13 @@ function HealthBar:render()
         love.graphics.draw(GTextures['main'], self.heartQuad, tempX, self.y)
         tempX = tempX + self.gap
     end
-    tempX = 0
+
+    tempX = self.x + 22
+
+    for i = 1, math.abs(self.totalHearts - self.remainingHearts), 1 do
+        love.graphics.draw(GTextures['main'], self.emptyHeartQuad, tempX, self.y)
+        tempX = math.abs(tempX - self.gap)
+    end
+
+    tempX = self.x
 end
